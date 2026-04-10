7075c77ace1b7675a0b97f4d354929beb335f681
Soubhanik Patra <soubhanikpatra@gmail.com>
2026-04-10 21:48:27 +0530

    graders added #2

--- email_triage_env/__init__.py
+++ email_triage_env/__init__.py
@@ -1,4 +1,5 @@
 # email_triage_env package
 from email_triage_env.models import TriageAction, EmailObservation, TriageState
+from email_triage_env.grader import grade
 
-__all__ = ["TriageAction", "EmailObservation", "TriageState"]
+__all__ = ["TriageAction", "EmailObservation", "TriageState", "grade"]
